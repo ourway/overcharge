cb3febcc1ea180acf04cfda697427e88206c6a31
Farsheed <ashouri@rashavas.com>
2017-06-05 15:28:26 +0430

    debugging

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 MIX_ENV=prod mix deps.get
-MIX_ENV=prod mix deps.update --all
+#MIX_ENV=prod mix deps.update --all
 MIX_ENV=prod mix deps.compile
 MIX_ENV=prod mix compile
 MIX_ENV=prod mix ecto.create
 MIX_ENV=prod mix ecto.migrate
 node_modules/brunch/bin/brunch build --production
 MIX_ENV=prod mix phoenix.digest
-MIX_ENV=prod PORT=4000 DOMAIN=chargell.ir mix run priv/scripts/add_pins.exs
+#MIX_ENV=prod PORT=4000 DOMAIN=chargell.ir mix run priv/scripts/add_pins.exs
 MIX_ENV=prod PORT=4000 DOMAIN=chargell.ir mix phoenix.server
 
