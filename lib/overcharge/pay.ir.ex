9a6bb47c5021b8ab2ff3215152f130dc23fbfef4
Farsheed <ashouri@rashavas.com>
2017-06-09 08:06:16 +0430

    good

--- lib/overcharge/pay.ir.ex
+++ lib/overcharge/pay.ir.ex
@@ -12,7 +12,7 @@ defmodule Overcharge.Pay do
             "amount" =>         invoice.amount * 10,
             "redirect" =>       "https://chargell.ir/deliver/#{invoice.uuid}",
             "factorNumber" =>   invoice.refid
-        } |> Poison.encode
+        } |> Poison.encode |> IO.inspect
 
        {:ok,  %HTTPoison.Response{body: body}} = HTTPoison.post("#{@baseurl}/send", payload, 
 					                    [{"Content-Type", "application/json"}], [])
