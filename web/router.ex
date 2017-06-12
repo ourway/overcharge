b933bcc233779e76ab095cb823d5bc79b83a5c14
Farsheed <ashouri@rashavas.com>
2017-06-12 23:32:32 +0430

    testing webhooks 2

--- web/router.ex
+++ web/router.ex
@@ -114,7 +114,7 @@ defmodule Overcharge.Router do
 
   scope "/bot", Overcharge do
     pipe_through :api
-    post "/381106026:AAFOWEEkwc4HWgQLYtr0rTGBdPzYhSHsFsw", ApiController, :echo
+    post "/{bot_token}", ApiController, :echo
   end
 
   scope "/admin", Overcharge do
